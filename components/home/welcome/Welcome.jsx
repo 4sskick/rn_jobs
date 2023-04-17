c4bc8163035f3826a78eb98cf6b5ea866292d7a0
Septian Adi Wijaya <developadi@gmail.com>
2023-04-17 17:00:18 +0700

    component - screen welcome

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 
 import styles from './welcome.style'
+import { useRouter } from 'expo-router'
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Welcome</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View styles={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
     </View>
   )
 }
