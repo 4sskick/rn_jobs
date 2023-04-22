99e0e55949b4c44ac460432951b7206858c1347b
Septian Adi Wijaya <developadi@gmail.com>
2023-04-23 02:10:23 +0700

    nearby - setup

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator } from 'react-native'
 
 import styles from './nearbyjobs.style'
+import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard'
+import { useRouter } from 'expo-router'
+import { COLORS } from '../../../constants'
+import useFetch from '../../../service/api/useFetch'
 
 const Nearbyjobs = () => {
+  const router = useRouter();
+
+  //calling object exported from class by passing param of endpoint & query
+  const { data, isLoading, error } = useFetch(
+    'search',
+    {
+      query: ' React native',
+      num_page: 1
+    }
+  );
+
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
+    <View style={styles.container}>
+
+      {/* header */}
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* actual container */}
+      <View style={styles.cardsContainer}>
+        {/* dynamic block of code */}
+        {isLoading ? (<ActivityIndicator size={'large'} colors={COLORS.primary} />)
+          : error ? (<Text>Something went wrong</Text>)
+            : (
+              data?.map((job) => (
+                <NearbyJobCard
+
+                />
+              ))
+            )
+        }
+      </View>
     </View>
   )
 }
